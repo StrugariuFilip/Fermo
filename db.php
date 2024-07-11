1441675d598580a527ea14f07554935689d2df7f
StrugariuFilip <166940507+StrugariuFilip@users.noreply.github.com>
2024-07-11 12:08:08 +0300

    Update db.php

--- db.php
+++ db.php
@@ -1,21 +1,21 @@
 <?php
-$servername = "server39.web-hosting.com";
-$username = "fermddgw_Filip";
-$password = "Deltaplan2006";
+$servername = "";
+$username = "";
+$password = "";
 
-$database = "fermddgw_register";
+$database = "";
 $con = new mysqli($servername, $username, $password, $database);
 if ($con->connect_error) {
     die("Connection failed: " . $con->connect_error);
 }
 
-$database2 = "fermddgw_Oferte";
+$database2 = "";
 $con2 = new mysqli($servername, $username, $password, $database2);
 if ($con2->connect_error) {
     die("Connection failed: " . $con2->connect_error);
 }
 
-$database3 = "fermddgw_chat";
+$database3 = "";
 $con3 = new mysqli($servername, $username, $password, $database3);
 if ($con3->connect_error) {
     die("Connection failed: " . $con3->connect_error);
